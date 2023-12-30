e589ca72979e04610722d96b5ee60b512181901f
EwelinaManiecka <emaniecka3@gmail.com>
2023-12-30 21:39:32 +0100

    add base form to login and registrtion

--- src/_auth/forms/SigninForm.tsx
+++ src/_auth/forms/SigninForm.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
+import * as z from "zod";
 
-// import { Button } from "@/components/ui/button";
+import { Button } from "@/components/ui/button";
+ 
 
 
 const SigninForm = () => {
+  const formSchema = z.object({
+    username: z.string().min(2).max(50),
+  })
+  
   return (
     <div>
       <div>działą</div>
-      {/* <Button>Click me</Button> */}
+      <Button>Click me</Button>
     </div>
   )
 }
