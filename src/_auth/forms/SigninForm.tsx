be4213824836618115a5c94bfaf839cd334abe1a
EwelinaManiecka <emaniecka3@gmail.com>
2023-12-30 21:47:32 +0100

    add comment code with form - mistake in deploy

--- src/_auth/forms/SigninForm.tsx
+++ src/_auth/forms/SigninForm.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import * as z from "zod";
+// import * as z from "zod";
 
 import { Button } from "@/components/ui/button";
  
 
 
 const SigninForm = () => {
-  const formSchema = z.object({
-    username: z.string().min(2).max(50),
-  })
-  
+  // const formSchema = z.object({
+  //   username: z.string().min(2).max(50),
+  // })
+
   return (
     <div>
       <div>działą</div>
